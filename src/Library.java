f95f5d4a4550bbdaa987fc70ba40371cd8e801e1
Vibudha94 <bvibuddha@gmail.com>
2024-07-25 00:03:04 +0530

    Create the projrct and add the libraries

---
+++ src/Library.java
@@ -0,0 +1,18 @@
+import java.net.URL;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Library extends Application {
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+    
+    }
+}
